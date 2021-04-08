259446ae9d59d5ae14a2ebe1c86e0f545c763b00
Ankita5892 <51524483+Ankita5892@users.noreply.github.com>
2021-04-09 00:48:20 +0530

    nn

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, this is ankita, and this is Github action CICD !"
+    return "Hello, this is dev enviroment, and Github action CICD !"
 
 
 if __name__ == "__main__":
