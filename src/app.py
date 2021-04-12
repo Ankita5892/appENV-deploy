28543311c3168af3fe951e48c540b796100b8627
Ankita5892 <51524483+Ankita5892@users.noreply.github.com>
2021-04-12 17:52:47 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 
-    return "Hello, Github action CICD from master !"
+    return "Hello, Github !"
 
 if __name__ == "__main__":
     app.run()
